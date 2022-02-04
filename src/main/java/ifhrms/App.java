e79265e0fe21b5c817bea02a53cf136ecda09ecb
vijayarathinam <vijayarathinamawsdevops@gmail.com>
2022-02-04 12:21:43 +0530

    mycommit

---
+++ src/main/java/ifhrms/App.java
@@ -0,0 +1,13 @@
+package ifhrms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to IFHRMS!" );
+    }
+}
